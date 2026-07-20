2bcf1d5a31e97bc417d009ccfd9e2eb1ab6801be
isabella <bellah.souza2009@gmail.com>
2026-07-20 16:13:02 -0300

    atualizacao

--- server.js
+++ server.js
@@ -24,3 +24,43 @@ app.use('/api', rotasAuth);
 app.listen(PORT, () => {
   console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
+
+
+
+
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+// ==========================================
+// 4. ROTA PARA DELETAR (EXCLUIR)
+// ==========================================
+app.delete('/api/itens/:id', (req, res) => {
+  const lista = lerArquivo();
+  const idProcurado = parseInt(req.params.id);
+
+  let posicao = -1;
+
+  // Procura em qual posição do Array o ID está
+  for (let i = 0; i < lista.length; i++) {
+    if (lista[i].id === idProcurado) {
+      posicao = i; // Guarda a posição (ex: 0, 1, 2...)
+      break;
+    }
+  }
+
+  if (posicao === -1) {
+    return res.json({ erro: "Item não encontrado!" });
+  }
+
+  // O comando .splice() remove o item usando a posição dele
+  lista.splice(posicao, 1);
+  
+  salvarArquivo(lista); // Salva a lista sem o item removido
+  res.json({ mensagem: "Excluído com sucesso!" });
+});
+
+// LIGA O SERVIDOR
+app.listen(3000, () => {
+  console.log("🚀 Servidor rodando em http://localhost:3000");
+});
